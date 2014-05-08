d4f59318df277b3ba9edd04f4f8a462339a65aa9
unknown <popsung1994@hotmail.com>
2014-05-08 02:59:31 -0700

    AddMoreAnimation

--- src/Item.js
+++ src/Item.js
@@ -3,11 +3,21 @@ var Item = cc.Sprite.extend({
     this.map = map;
     this.cat = map.cat;
 		this._super();
+    this.idle();
 		this.initWithFile('images/Coin.png');
     
 	},
+   
+	idle : function(){
+    var itemAnimation = new cc.Animation.create();
+    itemAnimation.addSpriteFrameWithFile('images/Coin.png');
+    itemAnimation.addSpriteFrameWithFile('images/Coin2.png');
+    itemAnimation.setDelayPerUnit( 0.3 );
 
-	
+    this.action = new cc.RepeatForever.create( cc.Animate.create( itemAnimation ) );
+    this.runAction(this.action);
+  },
+
    hit:function(cat){
       var catPosX = cat.getPositionX();
       var catPosY = cat.getPositionY();
@@ -28,9 +38,26 @@ var Item = cc.Sprite.extend({
       
 },
   update : function(dt){
-  
+    
     if(this.hit(this.cat)){
-      this.getParent().removeChild(this);
+      
+
+      // var myPosX = this.map.getPositionX() + this.getPositionX();
+      // var myPosY = this.map.getPositionY() +this.getPositionY();
+
+    var itemAnimation = new cc.Animation.create();
+    itemAnimation.addSpriteFrameWithFile('images/CoinAnimate.png');
+    //itemAnimation.setPosition(cc.p(myPosX,myPosY));
+    itemAnimation.setDelayPerUnit( 100 );
+
+    this.action2 = cc.Animate.create( itemAnimation ) ;
+    this.runAction(this.action2);
+
+    this.scheduleOnce(function(){
+      this.removeFromParent();
+    },0.1);
+
+
     }
   }
 
